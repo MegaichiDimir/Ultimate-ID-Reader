c04caddd0613743ec4d4ccb9212f648c70195ce5
Megaichi <92149111+MegaichiDimir@users.noreply.github.com>
2022-08-01 14:34:05 +0900

    CamPreviewをCaptureに統合
    カメラ表示機能を実装したが、レンダリングサイズでスクリーンショットが撮られるためreact-webcam自体の変更を検討

--- src/features/capture/Capture.tsx
+++ src/features/capture/Capture.tsx
@@ -1,17 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
+import Webcam from "react-webcam";
+import { css } from "@emotion/react";
 
-import CamPreview from "./CamPreview";
 import VideoList from "./VideoList";
 
 const Capture = () => {
 	const [deviceID, setDeviceID] = useState(null);
+	const [capImage, setCapImage] = useState(null);
+	const webcamRef = useRef<Webcam>(null);
+	const capture = useCallback(
+		() => {
+			const imageSrc = webcamRef.current.getScreenshot(); // imageSrcにBase64形式でpngを格納
+			if(imageSrc){
+				setCapImage(imageSrc);
+			}
+		},
+		[webcamRef]
+	);
+
+	const style = css`
+		
+	` 
 	
 	return (
 		<>
+			<Webcam
+				audio={false}
+				height={1080}
+				width={1920}
+				imageSmoothing={false}
+				ref={webcamRef}
+				screenshotFormat={'image/png'}
+				videoConstraints={{
+					width: 1920,
+					height: 1080,
+					facingMode: "user",
+					deviceId: deviceID
+				}}
+				css={style}
+
+			/>
+			<button onClick={capture}>Capture photo</button>
 			<VideoList
 				setDeviceID={setDeviceID}
 			/>
-			<div>device ID: {deviceID}</div>
+			<img src={capImage} alt="" />
 		</>
 	);
 };

2216df8bb5638dff44229e38b8a4f93c138c14d8
Megaichi <92149111+MegaichiDimir@users.noreply.github.com>
2022-08-04 16:59:05 +0900

    OCRコンポーネントのディレクトリ・パッケージを追加

---
+++ src/features/ocr/Jimp.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {
+	capImage: string
+	setOcrImage: React.Dispatch<React.SetStateAction<string>>
+}
+
+const Jimp:React.FC<Props> = (props) => {
+	
+
+	return(
+		<>
+		</>
+	)
+}
+
+export default Jimp;

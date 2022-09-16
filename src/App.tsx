8353698d945c6f04081d3639fe814188f8b2897c
Megaichi <92149111+MegaichiDimir@users.noreply.github.com>
2022-09-16 17:51:52 +0900

    レスポンシブ対応

--- src/App.tsx
+++ src/App.tsx
@@ -25,17 +25,26 @@ const App: React.FC = () => {
 
 	return(
 		<>
-			<div  css={[tw`container mx-auto max-w-screen-lg`, css`width: 640px;`]}>
-				<Capture 
-					setCapImage={setCapImage}
-					webcamRef={webcamRef}
-					capture={capture}
-				/>
-				<Ocr
-					capImage={capImage}
-				/>
-				<button css={tw`bg-blue-500 text-white`} onClick={capture}>Read ID</button>
-				{/* <img src={capImage} alt="" /> */}
+			<div  css={[tw`container mx-auto mt-4`, css``]}>
+				<div css={tw`mx-auto flex flex-col lg:flex-row flex-wrap gap-4 items-stretch`}>
+					<div css={tw`mx-auto flex-1 lg:basis-1/3 w-full`}>
+						<Capture 
+							setCapImage={setCapImage}
+							webcamRef={webcamRef}
+							capture={capture}
+						/>
+					</div>
+					<div css={tw`mx-auto flex flex-col items-stretch grow`}>
+						<div css={tw`w-full mx-auto max-w-screen-sm flex flex-col items-center`}>
+							<Ocr
+								capImage={capImage}
+							/>
+						</div>
+						<button css={tw`bg-blue-500 hover:bg-blue-400 text-white text-2xl rounded px-4 py-3 flex-none`} onClick={capture}>
+							Read ID
+						</button>
+					</div>
+				</div>
 			</div>
 		</>
 	);

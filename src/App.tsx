50dc2a16789e8e72299fed3986cc012ae865b7e2
Megaichi <92149111+MegaichiDimir@users.noreply.github.com>
2022-09-18 23:31:46 +0900

    flexbox関連を中心にスタイル微調整

--- src/App.tsx
+++ src/App.tsx
@@ -26,23 +26,23 @@ const App: React.FC = () => {
 
 	return(
 		<>
-			<div  css={[tw`container mx-auto mt-4`, css``]}>
-				<Header />
-				<div css={tw`mx-auto flex flex-col lg:flex-row flex-wrap gap-4 items-stretch`}>
-					<div css={tw`mx-auto flex-1 lg:basis-1/3 w-full`}>
+			<Header />
+			<div css={tw`px-2 mt-4 text-slate-800`}>
+				<div css={tw`container mx-auto flex flex-col lg:flex-row flex-wrap gap-4 items-stretch`}>
+					<div css={tw`w-full lg:max-w-screen-sm mx-auto flex-1 lg:basis-[49%]`}>
 						<Capture 
 							setCapImage={setCapImage}
 							webcamRef={webcamRef}
 							capture={capture}
-						/>
+						/> 
 					</div>
-					<div css={tw`mx-auto flex flex-col items-stretch grow`}>
-						<div css={tw`w-full mx-auto max-w-screen-sm flex flex-col items-center`}>
+					<div css={tw`lg:max-w-screen-sm w-full mx-auto flex flex-col items-stretch justify-between lg:basis-[49%]`}>
+						<div css={tw`w-full mx-auto flex flex-col items-center lg:gap-2`}>
 							<Ocr
 								capImage={capImage}
 							/>
 						</div>
-						<button css={tw`bg-blue-500 hover:bg-blue-400 text-white text-2xl rounded px-4 py-3 flex-none`} onClick={capture}>
+						<button css={tw`bg-blue-500 hover:bg-blue-400 text-white text-2xl rounded px-4 py-3 lg:py-3 mb-4 flex-none`} onClick={capture}>
 							Read ID
 						</button>
 					</div>

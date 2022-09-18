f29b9598ae3b78ba759177954c231a7c9a55b2de
Megaichi <92149111+MegaichiDimir@users.noreply.github.com>
2022-09-18 23:31:46 +0900

    画像処理前にキャプチャ画像の存在チェックを追加

--- src/features/ocr/process/ImgProc.tsx
+++ src/features/ocr/process/ImgProc.tsx
@@ -19,16 +19,19 @@ const ImgProc:React.FC<Props> = (props) => {
 
 		const getTxtImg = async () => {
 			const image = await Image.load(props.capImage);
-			const ocrImg = image
-				.crop({y:100})
-				.flipY()
-				.crop({x:1798, y:950})
-				.flipY()
-				.grey()	//グレースケール化
-				//.mask({threshold:0.6, invert:true}) //二値化
-				.invert() //色反転
-				.toBase64('image/png');
-			props.setOcrImage("data:image/png;base64," + ocrImg);
+			if(image){
+				const ocrImg = image
+					.crop({y:100})
+					.flipY()
+					.crop({x:1798, y:950})
+					.flipY()
+					.grey()	//グレースケール化
+					//.mask({threshold:0.6, invert:true}) //二値化
+					.invert() //色反転
+					.toBase64('image/png');
+				props.setOcrImage("data:image/png;base64," + ocrImg);
+			}
+
 		}
 		getTxtImg();
 	}, [props.capImage]);

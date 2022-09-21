911b234740d8c8c49046820d874a252aebb392ed
Megaichi <92149111+MegaichiDimir@users.noreply.github.com>
2022-09-21 19:24:52 +0900

    画像ファイルをバンドルするよう変更、file-loader削除

--- webpack.config.js
+++ webpack.config.js
@@ -33,12 +33,7 @@ module.exports = {
 			},
 			{
 				test: /\.(png|jp(e*)g|svg|gif)$/,
-				use: [{
-					loader: 'file-loader',
-					options: {
-						name: 'images/[name].[ext]',
-					},
-				}],
+				type: "asset/inline",
 			},
 		]
 	},

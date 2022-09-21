cdd934e5e26245354227a27593f1bbfc82757a61
Megaichi <92149111+MegaichiDimir@users.noreply.github.com>
2022-09-21 18:40:36 +0900

    タイトルをsvg要素に変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import tw,{ css } from "twin.macro";
-import {FaTwitter, FaGithub} from 'react-icons/fa'
+import {FaTwitter, FaGithub} from 'react-icons/fa';
+
+import Title from '../assets/img/title.svg';
 
 const Header: React.FC = () => {
 	
@@ -9,7 +11,7 @@ const Header: React.FC = () => {
 		<>
 			<div css={tw`bg-white px-2 py-4 w-full lg:mb-4 drop-shadow-md text-slate-800`}>
 				<div css={tw`container flex flex-row items-center justify-between mx-auto`}>
-					<h1>Ultimate ID Reader</h1>
+					<h1><img src={Title} css={tw`text-slate-500`} width="190" alt="Ultimate ID Reader" /></h1>
 					<div css={tw`flex flex-row gap-5 text-2xl text-slate-400`}>
 						<a href="https://twitter.com/Megaichi_Kenty" css={tw`hover:text-slate-600`}><FaTwitter/></a>
 						<a href="https://github.com/MegaichiDimir/SSBU-Room-ID-Reader" css={tw`hover:text-slate-600`}><FaGithub/></a>

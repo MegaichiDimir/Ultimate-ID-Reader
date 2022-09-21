b5a9668c1b561e8eb81b9d546517c466f779761d
Megaichi <92149111+MegaichiDimir@users.noreply.github.com>
2022-09-21 18:40:36 +0900

    タイトルをsvg要素に変更

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,15 @@ module.exports = {
 					},
 				]
 			},
+			{
+				test: /\.(png|jp(e*)g|svg|gif)$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						name: 'images/[name].[ext]',
+					},
+				}],
+			},
 		]
 	},
 	devServer: {
